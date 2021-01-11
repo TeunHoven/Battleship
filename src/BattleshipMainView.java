b8acb627b04cc11afac67c074c4ca56e68fb00da
Teun Hoven <t.d.hoven@student.utwente.nl>
2021-01-11 13:39:19 +0100

    To start the program and first file!

--- src/BattleshipMainView.java
+++ src/BattleshipMainView.java
@@ -1,2 +1,15 @@
-public class BattleshipMainView {
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class BattleshipMainView extends Application{
+    private static Stage mainStage;
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        mainStage = stage;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
