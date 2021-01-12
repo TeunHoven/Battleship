b952b8027730a24054e2023cbb902323b3cccc4f
Tim Eichhorn <t.s.eichhorn@student.utwente.nl>
2021-01-12 14:38:34 +0100

    Alle shipLives en shipLength ingevuld + player stub methods gemaakt

--- src/model/ship/DestroyerShip.java
+++ src/model/ship/DestroyerShip.java
@@ -2,8 +2,7 @@ package model.ship;
 
 public class DestroyerShip extends Ship {
 
-    public DestroyerShip() {
-        shipLength = 3;
-        shipLives = 3;
+    public DestroyerShip(int posX, int posY) {
+        super(3,3, posX ,posY);
     }
 }
