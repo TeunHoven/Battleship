a5bcc60b6f24c426b61e2c76a41c0b67c4628b88
Tim Eichhorn <t.s.eichhorn@student.utwente.nl>
2021-01-28 13:45:38 +0100

    Protocol start

--- src/Protocol/Client.java
+++ src/Protocol/Client.java
@@ -1,4 +1,278 @@
 package Protocol;
 
-public class Client {
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+
+    /**
+     * Client for BattleShip
+     */
+    public class Client {
+
+        private Socket serverSock;
+        private BufferedReader in;
+        private BufferedWriter out;
+
+        /**
+         * Constructs a new Client. Initialises the view.
+         */
+        public Client() {
+            serverSock = null;
+            in = null;
+            out = null;
+        }
+
+        /**
+         * Starts a new Client by creating a connection, followed by the
+         * HELLO handshake as defined in the protocol. After a successful
+         * connection and handshake, the view is started. The view asks for
+         * used input and handles all further calls to methods of this class.
+         *
+         * When errors occur, or when the user terminates a server connection, the
+         * user is asked whether a new connection should be made.
+         */
+        public void start() {
+            try {
+                createConnection();
+            } catch (ExitProgram e) {
+                e.printStackTrace();
+            }
+        }
+
+        /**
+         * Creates a connection to the server. Requests the IP and port to
+         * connect to at the view (TUI).
+         *
+         * The method continues to ask for an IP and port and attempts to connect
+         * until a connection is established or until the user indicates to exit
+         * the program.
+         *
+         * @throws ExitProgram if a connection is not established and the user
+         * 				       indicates to want to exit the program.
+         * @ensures serverSock contains a valid socket connection to a server
+         */
+        public void createConnection() throws ExitProgram {
+            clearConnection();
+            while (serverSock == null) {
+                String host = "127.0.0.1";
+                int port = 8888;
+
+                // try to open a Socket to the server
+                try {
+                    InetAddress addr = InetAddress.getByName(host);
+                    System.out.println("Attempting to connect to " + addr + ":"
+                            + port + "...");
+                    serverSock = new Socket(addr, port);
+                    in = new BufferedReader(new InputStreamReader(
+                            serverSock.getInputStream()));
+                    out = new BufferedWriter(new OutputStreamWriter(
+                            serverSock.getOutputStream()));
+                } catch (IOException e) {
+                    System.out.println("ERROR: could not create a socket on "
+                            + host + " and port " + port + ".");
+
+                    //Do you want to try again? (ask user, to be implemented)
+                    if(false) {
+                        throw new ExitProgram("User indicated to exit.");
+                    }
+                }
+            }
+        }
+
+        /**
+         * Resets the serverSocket and In- and OutputStreams to null.
+         *
+         * Always make sure to close current connections via shutdown()
+         * before calling this method!
+         */
+        public void clearConnection() {
+            serverSock = null;
+            in = null;
+            out = null;
+        }
+
+        /**
+         * Sends a message to the connected server, followed by a new line.
+         * The stream is then flushed.
+         *
+         * @param msg the message to write to the OutputStream.
+         * @throws ServerUnavailableException if IO errors occur.
+         */
+        public synchronized void sendMessage(String msg)
+                throws ServerUnavailableException {
+            if (out != null) {
+                try {
+                    out.write(msg);
+                    out.newLine();
+                    out.flush();
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                    throw new ServerUnavailableException("Could not write "
+                            + "to server.");
+                }
+            } else {
+                throw new ServerUnavailableException("Could not write "
+                        + "to server.");
+            }
+        }
+
+        /**
+         * Reads and returns one line from the server.
+         *
+         * @return the line sent by the server.
+         * @throws ServerUnavailableException if IO errors occur.
+         */
+        public String readLineFromServer()
+                throws ServerUnavailableException {
+            if (in != null) {
+                try {
+                    // Read and return answer from Server
+                    String answer = in.readLine();
+                    if (answer == null) {
+                        throw new ServerUnavailableException("Could not read "
+                                + "from server.");
+                    }
+                    return answer;
+                } catch (IOException e) {
+                    throw new ServerUnavailableException("Could not read "
+                            + "from server.");
+                }
+            } else {
+                throw new ServerUnavailableException("Could not read "
+                        + "from server.");
+            }
+        }
+
+        /**
+         * Reads and returns multiple lines from the server until the end of
+         * the text is indicated using a line containing ProtocolMessages.EOT.
+         *
+         * @return the concatenated lines sent by the server.
+         * @throws ServerUnavailableException if IO errors occur.
+         */
+        public String readMultipleLinesFromServer()
+                throws ServerUnavailableException {
+            if (in != null) {
+                try {
+                    // Read and return answer from Server
+                    StringBuilder sb = new StringBuilder();
+                    for (String line = in.readLine(); line != null
+                            && !line.equals(ProtocolMessages.EOT);
+                         line = in.readLine()) {
+                        sb.append(line + System.lineSeparator());
+                    }
+                    return sb.toString();
+                } catch (IOException e) {
+                    throw new ServerUnavailableException("Could not read "
+                            + "from server.");
+                }
+            } else {
+                throw new ServerUnavailableException("Could not read "
+                        + "from server.");
+            }
+        }
+
+        /**
+         * Closes the connection by closing the In- and OutputStreams, as
+         * well as the serverSocket.
+         */
+        public void closeConnection() {
+            System.out.println("Closing the connection...");
+            try {
+                in.close();
+                out.close();
+                serverSock.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void handleHello()
+                throws ServerUnavailableException, ProtocolException {
+            String message = "" + ProtocolMessages.HELLO;
+            sendMessage(message);
+
+            message = null;
+            while(message == null) {
+                message = readLineFromServer();
+            }
+
+            String[] args = message.split(ProtocolMessages.DELIMITER);
+            if(!message.startsWith(ProtocolMessages.HELLO + ProtocolMessages.DELIMITER)) {
+                throw new ProtocolException("No valid response");
+            } else {
+                System.out.println("> Welcome to the Hotel booking system of hotel:");
+            }
+        }
+
+        @Override
+        public void doIn(String guestName) throws ServerUnavailableException {
+            String message = "> Received: " + ProtocolMessages.IN + ProtocolMessages.DELIMITER + guestName + "\r\n";
+            System.out.print(message);
+        }
+
+        @Override
+        public void doOut(String guestName) throws ServerUnavailableException {
+            String message = "> Received: " + ProtocolMessages.OUT + ProtocolMessages.DELIMITER + guestName + "\r\n";
+            System.out.print(message);
+        }
+        @Override
+        public void doRoom(String guestName) throws ServerUnavailableException {
+            String message = "> Received: " + ProtocolMessages.ROOM + ProtocolMessages.DELIMITER + guestName + "\r\n";
+            System.out.print(message);
+        }
+        @Override
+        public void doAct(String guestName, String password)
+                throws ServerUnavailableException {
+            String message = "> Received: " + ProtocolMessages.ACT + ProtocolMessages.DELIMITER + guestName
+                    + ProtocolMessages.DELIMITER + password + "\r\n";
+            System.out.print(message);
+        }
+
+        @Override
+        public void doBill(String guestName, String nights)
+                throws ServerUnavailableException {
+            int nightsInt = 0;
+            try {
+                nightsInt = Integer.parseInt(nights);
+            } catch (NumberFormatException e) {
+                System.out.println("Number of nights was not an integer!");
+            }
+            if(nightsInt > 0) {
+                String message = "> Received: " + ProtocolMessages.BILL + ProtocolMessages.DELIMITER + guestName + ProtocolMessages.DELIMITER + nights + "\r\n";
+                System.out.print(message);
+            } else {
+                System.out.println("nights is negative!");
+            }
+        }
+
+        @Override
+        public void doPrint() throws ServerUnavailableException {
+            String message = "> Received: " + ProtocolMessages.PRINT + "\r\n";
+            System.out.print(message);
+        }
+
+        @Override
+        public void sendExit() throws ServerUnavailableException {
+            String message ="> Received: " + ProtocolMessages.EXIT + "\r\n";
+            System.out.print(message);
+            closeConnection();
+        }
+
+        /**
+         * This method starts a new HotelClient.
+         *
+         * @param args
+         */
+        public static void main(String[] args) {
+            (new HotelClient()).start();
+        }
+
+    }
+
 }
---
+++ src/Protocol/ClientHandler.java
@@ -0,0 +1,102 @@
+package Protocol;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+
+    /**
+    * ClientHandler for the BattleShip client.
+    * This class can handle the communication with one
+    * client.
+    */
+public class ClientHandler implements Runnable {
+
+        /** The socket and In- and OutputStreams */
+        private BufferedReader in;
+        private BufferedWriter out;
+        private Socket sock;
+
+        /** The connected HotelServer */
+        private Server srv;
+
+        /** Name of this ClientHandler */
+        private String name;
+
+        /**
+         * Constructs a new ClientHandler. Opens the In- and OutputStreams.
+         *
+         * @param sock The client socket
+         * @param srv  The connected server
+         * @param name The name of this ClientHandler
+         */
+        public ClientHandler(Socket sock, Server srv, String name) {
+            try {
+                in = new BufferedReader(
+                        new InputStreamReader(sock.getInputStream()));
+                out = new BufferedWriter(
+                        new OutputStreamWriter(sock.getOutputStream()));
+                this.sock = sock;
+                this.srv = srv;
+                this.name = name;
+            } catch (IOException e) {
+                shutdown();
+            }
+        }
+
+        /**
+         * Continuously listens to client input and forwards the input to the
+         * {@link #handleCommand(String)} method.
+         */
+        public void run() {
+            String msg;
+            try {
+                msg = in.readLine();
+                while (msg != null) {
+                    System.out.println("> [" + name + "] Incoming: " + msg);
+                    handleCommand(msg);
+                    out.newLine();
+                    out.flush();
+                    msg = in.readLine();
+                }
+                shutdown();
+            } catch (IOException e) {
+                shutdown();
+            }
+        }
+
+        /**
+         * Handles commands received from the client by calling the according
+         * methods at the Server. For example, when the message "i Name"
+         * is received, the method doIn() of HotelServer should be called
+         * and the output must be sent to the client.
+         *
+         * If the received input is not valid, send an "Unknown Command"
+         * message to the server.
+         *
+         * @param msg command from client
+         * @throws IOException if an IO errors occur.
+         */
+        private void handleCommand(String msg) throws IOException {
+            // To be implemented
+        }
+
+        /**
+         * Shut down the connection to this client by closing the socket and
+         * the In- and OutputStreams.
+         */
+        private void shutdown() {
+            System.out.println("> [" + name + "] Shutting down.");
+            try {
+                in.close();
+                out.close();
+                sock.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            srv.removeClient(this);
+        }
+    }
+}
